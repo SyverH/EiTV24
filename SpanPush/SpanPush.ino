d0f6568c11697e3670452ce8267ddad07fe3bff5
Syver <Syverharaldsen@gmail.com>
2024-02-14 13:43:51 +0100

    Finished code for span-push

--- SpanPush/SpanPush.ino
+++ SpanPush/SpanPush.ino
@@ -2,17 +2,30 @@
 #include "arduino_secrets.h"
 
 #include <ArduinoJson.h>
+#include <ArduinoLowPower.h>
 
-const int drySensorValue = 498;
-const int wetSensorValue = 886;
+//#define DEBUG
 
-const int JsonSize = 128;
-
-const int groundMoisturePin = A1;
+#define SPAN_APN "mda.lab5e"
 
 const char PINNUMBER[] = SECRET_PINNUMBER;
 
-unsigned int localPort = 1234;      // local port to listen for UDP packets
+const int sleepTime = 60 * 1000; // 10 seconds
+
+/* Calibration-values for the soil sensor */
+const int drySensorValue = 498;
+const int wetSensorValue = 886;
+
+/* The maximum size of the JSON-formatted package */
+const int JsonSize = 128;
+
+
+const int groundMoisturePin = A1;
+
+
+/* Constants for connection to SPAN */
+unsigned int localPort = 4200;      // local port to listen for UDP packets
+unsigned int spanPort = 1234;
 
 IPAddress Lab5eSpanIP(172, 16, 15, 14);
 
@@ -39,60 +52,61 @@ void setup(){
         ;
     }
 
-    Serial.println("Starting Arduino GPRS NTP client.");
+    Serial.println("Connecting to SPAN...."); 
+    boolean connected = false;
 
-    boolean Connected = false;
-
-    while(!Connected){
-        if((nbAccess.begin(PINNUMBER) == NB_READY) &&
-            (gprs.attachGPRS() == GPRS_READY)){
-                Connected = true;
-            }
-        else{
-            Serial.println("NB not connected");
-            delay(1000);
-        }
+    while (!connected) {
+    if ((nbAccess.begin(PINNUMBER, SPAN_APN) == NB_READY) &&
+        (gprs.attachGPRS() == GPRS_READY)) {
+        connected = true;
+    } else {
+    Serial.println("Not connected, retrying...");
+    delay(1000);
     }
-
-    Serial.println("\nStarting connection to server...");
+  }
 
     Udp.begin(localPort);
 }
 
 void loop(){
 
-
-    delay(5000);
-    Serial.println("Sending data to Lab5eSpanIP ");
-
-    /*
-    This code should fill a json document with the groundMoisture value and send it to the Lab5eSpanIP
-    */
-
-    int sensorValue = readAnalogSensor(groundMoisturePin);
+    int sensorValue = analogRead(groundMoisturePin);
     int moisturePercentage = map(sensorValue, drySensorValue, wetSensorValue, 100, 0);
 
-    doc["Source"] = "Arduino MKR 1500 NB";
+    // Adds the data source to the json document
+    //doc["Source"] = "Arduino MKR 1500 NB";
     // Adds the groundMoisture value to the json document
-    doc["groundMoisture"] = moisturePercentage;
+    doc["Moisture"] = moisturePercentage;
 
+    /* Clear the messagePayload */
     char messagePayload[JsonSize] = "";
 
+    /* Debugging mode  */
+    #ifdef DEBUG
+
+    Serial.println("Sending data to Lab5eSpanIP\n");
+    
     serializeJson(doc, Serial);
     Serial.println("\n");
 
-    /*
+    delay(5000);
+
+    /* If not debug mode */
+    #else
+    
     serializeJson(doc, messagePayload) + "\n";
 
     Udp.beginPacket(Lab5eSpanIP, localPort);
 
-    Udp.write(messagePayload);
+    Udp.write(messagePayload, strlen(messagePayload));
 
     Udp.endPacket();
-    */
+
+    LowPower.sleep(sleepTime);
+    #endif
 }
 
-int readAnalogSensor(int Pin){
-    int sensorValue = analogRead(Pin);
-    return sensorValue;
+void serializePacket(StaticJsonDocument<JsonSize> doc, char *messagePayload){
+    serializeJson(doc, Serial);
+    Serial.println("\n");
 }
